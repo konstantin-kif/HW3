09dfc9b809288450fad86353bfd0e468f0e6f37d
Konstantin <konstantin.kif@mail.ru>
2022-08-19 01:43:22 +0700

    add task2

--- HW3/task2/Program.cs
+++ HW3/task2/Program.cs
@@ -1,3 +1,33 @@
 ﻿// Напишите программу, которая принимает на вход координаты двух точек и
 // находит расстояние между ними в 3D пространстве.
 
+const int X = 0;
+const int Y = 1;
+const int Z = 2;
+
+int[] InputCoords(string message)
+{
+    int[] numCoord = new int[3];
+    for (int i = 0; i < numCoord.Length; i++)
+    {
+        Console.WriteLine($"{message} {i}-я");
+        string value = Console.ReadLine();
+        numCoord[i] = int.Parse(value);
+    }
+    return numCoord;
+}
+
+double Power2FromPoint(double C1, double C2)
+{
+    double distanceCoord = C1 - C2;
+    return distanceCoord * distanceCoord;
+}
+double LineLong(int[] point1, int[] point2)
+{
+    double longLine = Math.Sqrt(Power2FromPoint(point1[X], point2[X]) + Power2FromPoint(point1[Y], point2[Y]) + Power2FromPoint(point1[Z], point2[Z]));
+    return longLine;
+}
+
+int[] point1 = InputCoords("Координаты первой точки ");
+int[] point2 = InputCoords("Координаты второй точки ");
+Console.Write($"Расcтояние между точками {LineLong(point1, point2)}");
